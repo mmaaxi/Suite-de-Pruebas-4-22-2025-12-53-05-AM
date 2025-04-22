5c2e2278f3e226b7326e8f96d0a16f3e17f2811a
Maximiliano Reyes López <113549509+mmaaxi@users.noreply.github.com>
2025-04-21 18:54:04 -0600

    agregando src/test/java/pages/tc7Page.java

---
+++ src/test/java/pages/tc7Page.java
@@ -0,0 +1,20 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class tc7Page {
+    WebDriver driver;
+
+    public tc7Page(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void realizaFlujoDeTrabajoY() {
+        // Lógica para realizar el flujo de trabajo Y
+    }
+
+    public boolean validarFlujoDeTrabajoY() {
+        // Validación del flujo de trabajo Y
+        return true;
+    }
+}

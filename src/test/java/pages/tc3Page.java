90e0f3425c609bd97f9cbc847f1d52ee98109ff7
Maximiliano Reyes López <113549509+mmaaxi@users.noreply.github.com>
2025-04-21 18:54:01 -0600

    agregando src/test/java/pages/tc3Page.java

---
+++ src/test/java/pages/tc3Page.java
@@ -0,0 +1,19 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class tc3Page {
+    WebDriver driver;
+
+    public tc3Page(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void simulateScenarioA() {
+        // Implementation for simulating scenario A
+    }
+
+    public void verifyBehaviorA() {
+        // Implementation for verifying behavior A
+    }
+}
